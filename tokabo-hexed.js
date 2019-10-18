0a89872882f91243719a04d82bc151a70ca8502a
ZhaoxiSun19991225 <47791231+ZhaoxiSun19991225@users.noreply.github.com>
2019-10-18 14:49:40 -0400

    Update tokabo-hexed.js

--- tokabo-hexed.js
+++ tokabo-hexed.js
@@ -85,6 +85,9 @@
       sliders_red_slider.type = "range";
       sliders_red_slider.max = "255";
       sliders_red_slider.value = "0";
+      sliders_red_slider.oninput = function(){
+          document.getElementById("red_number").value = this.value;
+      };
       sliders_red_slider.onchange = function(){
           document.getElementById("red_number").value = this.value;
       };
@@ -92,6 +95,11 @@
       sliders_red_number.id = "red_number";
       sliders_red_number.type = "number";
       sliders_red_number.value = "0";
+      sliders_red_number.min = "0";
+      sliders_red_number.max = "255";
+      sliders_red_number.oninput = function(){
+          document.getElementById("red_slider").value = this.value;
+      };
       sliders_red_number.onchange = function(){
           document.getElementById("red_slider").value = this.value;
       };
@@ -107,6 +115,9 @@
       sliders_green_slider.type = "range";
       sliders_green_slider.max = "255";
       sliders_green_slider.value = "0";
+      sliders_green_slider.oninput = function(){
+          document.getElementById("green_number").value = this.value;
+      };
       sliders_green_slider.onchange = function(){
           document.getElementById("green_number").value = this.value;
       };
@@ -114,6 +125,11 @@
       sliders_green_number.id = "green_number";
       sliders_green_number.type = "number";
       sliders_green_number.value = "0";
+      sliders_green_number.min = "0";
+      sliders_green_number.max = "255";
+      sliders_green_number.oninput = function(){
+          document.getElementById("green_slider").value = this.value;
+      };
       sliders_green_number.onchange = function(){
           document.getElementById("green_slider").value = this.value;
       };
@@ -129,6 +145,9 @@
       sliders_blue_slider.type = "range";
       sliders_blue_slider.max = "255";
       sliders_blue_slider.value = "0";
+      sliders_blue_slider.oninput = function(){
+          document.getElementById("blue_number").value = this.value;
+      };
       sliders_blue_slider.onchange = function(){
           document.getElementById("blue_number").value = this.value;
       };
@@ -136,6 +155,11 @@
       sliders_blue_number.id = "blue_number";
       sliders_blue_number.type = "number";
       sliders_blue_number.value = "0";
+      sliders_blue_number.min = "0";
+      sliders_blue_number.max = "255";
+      sliders_blue_number.oninput = function(){
+          document.getElementById("blue_slider").value = this.value;
+      };
       sliders_blue_number.onchange = function(){
           document.getElementById("blue_slider").value = this.value;
       };
